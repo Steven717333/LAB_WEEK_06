a222227484d2072bf606ed3b4ced34a1b8cd192a
Steven717333 <stevenlee313777@gmail.com>
2025-10-08 21:56:04 +0700

    Commit No.4 Tugas Modul Lab

--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -52,11 +52,61 @@ class MainActivity : AppCompatActivity() {
                     Gender.Unknown,
                     CatBreed.AmericanCurl,
                     "Curious George",
-                    "Award winning investigator",
+                    "Award-winning investigator",
                     "https://cdn2.thecatapi.com/images/bar.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.BalineseJavanese,
+                    "Snow",
+                    "A pure white cat who loves to sleep under sunlight",
+                    "https://cdn2.thecatapi.com/images/MTY3ODIyNQ.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.AmericanCurl,
+                    "Gary",
+                    "Always curious and loves to chase toy mice",
+                    "https://cdn2.thecatapi.com/images/9v6.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.ExoticShorthair,
+                    "Garfield",
+                    "Loves lasagna and naps all day long",
+                    "https://cdn2.thecatapi.com/images/MTY3ODIyMg.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.BalineseJavanese,
+                    "Buttercups",
+                    "Playful kitten who loves to climb everything",
+                    "https://cdn2.thecatapi.com/images/8dr.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.ExoticShorthair,
+                    "Muffin",
+                    "Sweet and cuddly, always purring near people",
+                    "https://cdn2.thecatapi.com/images/MTY3ODIyMQ.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.AmericanCurl,
+                    "Shadow",
+                    "Mysterious and sneaky, appears out of nowhere",
+                    "https://cdn2.thecatapi.com/images/a34.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.ExoticShorthair,
+                    "Oren",
+                    "The cheerful orange cat who loves everyone",
+                    "https://cdn2.thecatapi.com/images/MTY3ODIyMQ2.jpg"
                 )
             )
         )
+
         val itemTouchHelper = ItemTouchHelper(catAdapter.swipeToDeleteCallback)
         itemTouchHelper.attachToRecyclerView(recyclerView)
 
